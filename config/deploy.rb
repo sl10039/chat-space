8ba7228ef6198ded86dad9f76e9f47f978aa2dc9
sl10039 <black.d.yfc@i.softbank.jp>
2020-01-24 14:17:41 +0900

    aws

--- config/deploy.rb
+++ config/deploy.rb
@@ -31,18 +31,17 @@ set :linked_files, %w{ config/secrets.yml }
 after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
   task :restart do
-  
-end
+    invoke 'unicorn:restart'
+  end
 
-
-desc 'upload secrets.yml'
-task :upload do
-  on roles(:app) do |host|
-    if test "[ ! -d #{shared_path}/config ]"
-      execute "mkdir -p #{shared_path}/config"
-    end
-      upload!('config/secrets.yml', "#{shared_path}/config/secrets.yml")
-    end
+  desc 'upload secrets.yml'
+  task :upload do
+    on roles(:app) do |host|
+      if test "[ ! -d #{shared_path}/config ]"
+        execute "mkdir -p #{shared_path}/config"
+      end
+        upload!('config/secrets.yml', "#{shared_path}/config/secrets.yml")
+      end
   end
   before :starting, 'deploy:upload'
   after :finishing, 'deploy:cleanup'
