268317fdff7972d4945ac46681fa53133e644b2d
sl10039 <black.d.yfc@i.softbank.jp>
2020-01-24 10:20:16 +0900

    change

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -115,6 +115,6 @@ $('#new_message').on('submit', function(e){
   };
 
 if (document.location.href.match(/\/groups\/\d+\/messages/)) {
-  setInterval(reloadMessages, 1000);
+  setInterval(reloadMessages, 7000);
 } 
 });
